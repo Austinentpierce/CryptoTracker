8cc6476902b4904ea100c4eb3e92d437419c5f6b
Austin Pierce <Austinentpierce@gmail.com>
2021-09-14 19:23:14 -0400

    added different background

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
 import React from 'react'
 
-
 export function App() {
-  return <div>Hello, World</div>
+  return (
+    <main>
+      <header>
+        <i className="bitcoin fab fa-4x fa-bitcoin"></i>
+        <h1 className="title">Crypto Price Tracker</h1>
+        <i className="ethereum fab fa-4x fa-ethereum"></i>
+      </header>
+      <div className="instructions">
+        <h2>Instructions</h2>
+        <p>
+          This app was created to see current certain Crypto Currency prices and
+          the app will update every 10 seconds to see the updated price
+        </p>
+      </div>
+    </main>
+  )
 }
