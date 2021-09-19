5f32e5e6d81fb4522246d99696906cb798940622
Austin Pierce <Austinentpierce@gmail.com>
2021-09-19 16:05:44 -0400

    updates to css for more better user experience

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ export function App() {
   return (
     <main>
       <header>
-        <i className="bitcoin fab fa-4x fa-bitcoin"></i>
-        <h1 className="title">Crypto Price Tracker</h1>
-        <i className="ethereum fab fa-4x fa-ethereum"></i>
+        <i className="bitcoin fab fa-2x fa-bitcoin"></i>
+        <h1 className="title">Crypto Currency Price Tracker</h1>
+        <i className="ethereum fab fa-2x fa-ethereum"></i>
       </header>
       <div className="instructions">
-        <h2>Instructions</h2>
+        <h2 className="headinstruct">Instructions</h2>
         <p>
           This app was created to see current certain Crypto Currency prices and
           the app will update every 10 seconds to see the updated price
